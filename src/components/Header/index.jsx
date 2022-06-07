c6c0b1014e914118795e612b511e987b2497c912
Rafael Rodrigues <rafael-rafa2.1@hotmail.com>
2022-06-06 21:07:06 -0300

    Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,11 @@
+import igniteLogo from '../../assets/ignite-logo.svg';
+
 import styles from './styles.module.scss';
 
 export function Header() {
   return (
     <header className={styles.header}>
+      <img src={igniteLogo} alt='Logo Ignite' />
       <h1>Ignite Feed</h1>
     </header>
   );
