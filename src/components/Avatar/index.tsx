b21b81c2e9d6f14ebe900d8182735617b3d5a85d
Rafael Rodrigues <rafael-rafa2.1@hotmail.com>
2022-06-17 12:32:30 -0300

    Extends AvatarProps

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,17 +1,16 @@
+import { ImgHTMLAttributes } from 'react';
+
 import styles from './styles.module.scss';
 
-interface AvatarProps {
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement> {
   hasBorder?: boolean;
-  src: string;
-  alt?: string;
 }
 
-export function Avatar({ hasBorder = true, src, alt }: AvatarProps) {
+export function Avatar({ hasBorder = true, ...props }: AvatarProps) {
   return (
     <img
       className={hasBorder ? styles.avatarWithBorder : styles.avatar}
-      src={src}
-      alt={alt}
+      {...props}
     />
   );
 }
